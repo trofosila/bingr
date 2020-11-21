3bdead3d2906783a69c86bf4a4a538f96bceed0f
Laurentiu Trofosila <trofosila@gmail.com>
2020-11-21 13:10:46 +0100

    don't run as root

--- bingr.sh
+++ bingr.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [[ $EUID -eq 0 ]]; then
+    echo -ne '\007' # bell
+    echo "ERROR: This script must NOT be run as root" 1>&2
+    exit 1
+fi
+
 readonly SAVE_PATH="$HOME/Pictures/bingr"
 readonly BING="https://www.bing.com"
 readonly URL="$BING/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
